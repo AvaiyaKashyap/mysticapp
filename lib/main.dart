63e8f06c030ea60ac8672b8a89a9405079768087
Kashyap Avaiya <88942744+AvaiyaKashyap@users.noreply.github.com>
2023-11-03 18:04:11 +0530

    roughly basic design

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 import 'screens/welcomepage.dart';
 void main()
 {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    // theme: ThemeData(useMaterial3: true),
-    theme: FlexThemeData.light(scheme: FlexScheme.espresso),
-      themeMode: ThemeMode.system,
-    home: WelcomePage(),
+  runApp(Sizer(
+    builder: (BuildContext context, Orientation orientation, DeviceType deviceType) { 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(splashColor: Colors.transparent,useMaterial3: true),
+      // theme: ThemeData(useMaterial3: true),
+      // theme: FlexThemeData.light(scheme: FlexScheme.espresso,),
+      //   themeMode: ThemeMode.system,
+      home: WelcomePage(),
+    );
+    },
   ));
 }
--- lib/screens/welcomepage.dart
+++ lib/screens/welcomepage.dart
@@ -1,5 +1,7 @@
 import 'package:aura_box/aura_box.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 import 'package:tab_container/tab_container.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -10,19 +12,88 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+
+  String longtext = "Unlock the limitless potential of your ideas with our comprehensive app development service. Our team of seasoned developers is dedicated to crafting state-of-the-art iOS and Android applications tailored to your specific business requirements. We specialize in transforming your vision into a reality, from the initial concept to the final deployment. Our holistic approach ensures a seamless user experience and robust functionality that sets you apart in the digital landscape. We understand the ever-evolving technology landscape and stay at the forefront of industry trends to offer you the latest features and innovations";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(80.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Color(0xffFFF3DA),
-                  border: Border.all(width: 4),
-                  borderRadius: BorderRadius.circular(20)),
+        body: Stack(
+          children:[ 
+            Container(
+               height: (MediaQuery.of(context).size.height),
+        width: (MediaQuery.of(context).size.width),
+              child: Image.asset('assets/back1.png',fit: BoxFit.cover,)
+            ),
+            Center(
+            child: Padding(
+              padding: const EdgeInsets.all(60.0),
+              child: Container(
+                height: (MediaQuery.of(context).size.height),
+        width: (MediaQuery.of(context).size.width),
+                decoration: BoxDecoration(
+                    color: Color(0xffFFF3DA).withOpacity(0.8),
+                    border: Border.all(width: 4,),
+                    borderRadius: BorderRadius.circular(20)),
+                    alignment: Alignment.center,
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              height: 50.h,
+                              width: 25.w,
+                              //color: Colors.white,
+                              decoration: BoxDecoration(
+                          //       boxShadow: [
+                          //   BoxShadow(
+                          //     color: Colors.grey.withOpacity(0.5),
+                          //     spreadRadius: 5,
+                          //     blurRadius: 7,
+                          //     offset: Offset(0, 3), // changes position of shadow
+                          //   ),
+                          // ],
+                                image: DecorationImage(image: AssetImage('assets/logo1.png'))
+                              ),
+                            ),
+                            Container(
+                              height: 50.h,
+                              //color: Colors.green,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("MYSTICAPP",style: GoogleFonts.duruSans(textStyle: TextStyle(
+                                    fontSize: 10.sp,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff3B5060)
+                                  )),),
+                                  SizedBox(height: 1.h,),
+                                  Text("Unlock the Power of Mobile Apps with Our Expert Development Service",style: GoogleFonts.duruSans(textStyle: TextStyle(
+                                    fontSize: 4.sp,
+                                    fontWeight: FontWeight.w700,
+                                    color: Color(0xff3B5060)
+                                  )),),
+                                  SizedBox(height: 5.h,),
+                                  Container(
+                                  width: 50.w,
+                                    child: Text(longtext,overflow: TextOverflow.ellipsis,maxLines: 8,style:  GoogleFonts.duruSans(textStyle: TextStyle(
+                                      fontSize: 3.sp,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color(0xff3B5060)
+                                    )),))
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+              ),
             ),
           ),
+          ],
         ),
         backgroundColor: Colors.deepPurple[100]);
   }
